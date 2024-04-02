12ed79bf6440df48fea37fca564cdea893cc47af
Victor Kemboi <vickemtoo97@gmail.com>
2024-04-02 09:09:00 +0300

    modification of elements-DOM

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader =document.createElement("h1");
+newHeader.id = "victory";
+const Header= document.querySelector("h1");
+newHeader.textContent = "Welcome" + " " + "YOUR-NAME" + " " + "is the champion";
